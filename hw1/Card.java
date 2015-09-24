f0ba70ef0e913776be887945869d5ef23a8513d8
AdrianHsu <adrianhsu1995@gmail.com>
2015-09-24 23:34:38 +0800

    done

--- hw1/Card.java
+++ hw1/Card.java
@@ -3,5 +3,4 @@ public class Card extends BaseCard {
 	public Card(Ranks _rank, Suits _suit) {
 		super(_rank, _suit);
 	}
-	public boolean isJoker = false;
 }
--- hw1/JokerCard.java
+++ hw1/JokerCard.java
@@ -3,5 +3,4 @@ public class JokerCard extends BaseCard {
 	public JokerCard(Ranks _rank, Suits _suit) {
 		super(_rank, _suit);
 	}
-	public boolean isJoker = true;
 }
