d1aa3549e45761f9b7cb10fd0867aacc3f571ef3
AdrianHsu <adrianhsu1995@gmail.com>
2015-11-13 13:42:40 +0800

    one done

--- hw3/src/OldMaid.java
+++ hw3/src/OldMaid.java
@@ -1,4 +1,66 @@
+import java.util.ArrayList;
+
 public class OldMaid {
 
+	public static int NUM_OF_PLAYERS = 4;	
+
+	public static void dealCards(ArrayList<Player> _players, DeckOfCards _deckOfCards){};
 	
+	public static void sort(ArrayList<Player> _players) {
+		for(int i = 0; i < _players.size(); i++) {
+			_players.get(i).sortHand();
+		}
+	}
+	public static void cardsOutput(ArrayList<Player> _players) {
+		
+		for(int i = 0; i < _players.size(); i++) {
+			System.out.print("Player" + i + ": ");
+
+			for(int j = 0; j < _players.get(i).getHand().size() - 1; j++) {
+				System.out.print( _players.get(i).getHand().get(j).cardToString() + " ");
+			}
+			int  last = _players.get(i).getHand().size() - 1;
+			System.out.println( _players.get(i).getHand().get(last).cardToString());
+		}
+	}
+	public static void drawCardsOutput(ArrayList<Player> _players, int to, int from) {
+		
+		int toIndex = _players.get(to).getIndex();
+		int fromIndex = _players.get(from).getIndex();
+		//to
+		System.out.print("Player" + toIndex + ": ");
+		if(_players.get(to).getHand().size() != 0) {
+				
+			for(int j = 0; j < _players.get(to).getHand().size() - 1; j++) {
+				System.out.print( _players.get(to).getHand().get(j).cardToString() + " ");
+			}
+			int  last = _players.get(to).getHand().size() - 1;
+			System.out.println( _players.get(to).getHand().get(last).cardToString());
+		} else {
+			System.out.println("");
+		}
+		//from
+		System.out.print("Player" + fromIndex + ": ");
+		if(_players.get(from).getHand().size() != 0) {
+
+			for(int j = 0; j < _players.get(from).getHand().size() - 1; j++) {
+				System.out.print( _players.get(from).getHand().get(j).cardToString() + " ");
+			}
+			int last = _players.get(from).getHand().size() - 1;
+			System.out.println( _players.get(from).getHand().get(last).cardToString());
+		} else {
+			System.out.println("");
+		}
+	}
+	public static void drawing(Player pTo, Player pFrom, int index) {
+
+		Card drawCard = new Card( pFrom.getHand().get(index) );
+		pFrom.removeCard(index);
+		pTo.addCard(drawCard);
+		pFrom.sortHand();
+		pTo.sortHand();
+		pFrom.dropPairs();
+		pTo.dropPairs();
+	}
+
 }
--- hw3/src/VariantOne.java
+++ hw3/src/VariantOne.java
@@ -1,72 +1,22 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
+// A variant in East Asia is called baba-nuki (ババ抜き, "old maid") in Japan 
+// and dodukjapki (도둑잡기, "catching the thief") in Korea. 
+// It is played exactly as old maid, but instead of removing a queen or any other card, 
+// a joker is added, and player who is left with it loses.
 
 public class VariantOne extends OldMaid {
 
-
 	public static void dealCards(ArrayList<Player> _players, DeckOfCards _deckOfCards) {
 		for(int j = 0; j < 54 ; j++) {
-			_players.get(j % 4).getHand().add( _deckOfCards.totalCards.get(j) );
+			if( !(_deckOfCards.totalCards.get(j).getSuit() == Suits.BLACK) )
+				_players.get(j % 4).getHand().add( _deckOfCards.totalCards.get(j) );
 		}
 	}
-	public static void sort(ArrayList<Player> _players) {
-		for(int i = 0; i < _players.size(); i++) {
-			_players.get(i).sortHand();
-		}
-	}
-	public static void cardsOutput(ArrayList<Player> _players) {
-		
-		for(int i = 0; i < _players.size(); i++) {
-			System.out.print("Player" + i + ": ");
 
-			for(int j = 0; j < _players.get(i).getHand().size() - 1; j++) {
-				System.out.print( _players.get(i).getHand().get(j).cardToString() + " ");
-			}
-			int  last = _players.get(i).getHand().size() - 1;
-			System.out.println( _players.get(i).getHand().get(last).cardToString());
-		}
-	}
-	public static void drawCardsOutput(ArrayList<Player> _players, int to, int from) {
-		
-		int toIndex = _players.get(to).getIndex();
-		int fromIndex = _players.get(from).getIndex();
-		//to
-		System.out.print("Player" + toIndex + ": ");
-		if(_players.get(to).getHand().size() != 0) {
-				
-			for(int j = 0; j < _players.get(to).getHand().size() - 1; j++) {
-				System.out.print( _players.get(to).getHand().get(j).cardToString() + " ");
-			}
-			int  last = _players.get(to).getHand().size() - 1;
-			System.out.println( _players.get(to).getHand().get(last).cardToString());
-		} else {
-			System.out.println("");
-		}
-		//from
-		System.out.print("Player" + fromIndex + ": ");
-		if(_players.get(from).getHand().size() != 0) {
-
-			for(int j = 0; j < _players.get(from).getHand().size() - 1; j++) {
-				System.out.print( _players.get(from).getHand().get(j).cardToString() + " ");
-			}
-			int last = _players.get(from).getHand().size() - 1;
-			System.out.println( _players.get(from).getHand().get(last).cardToString());
-		} else {
-			System.out.println("");
-		}
-	}
-	public static void drawing(Player pTo, Player pFrom, int index) {
-
-		Card drawCard = new Card( pFrom.getHand().get(index) );
-		pFrom.removeCard(index);
-		pTo.addCard(drawCard);
-		pFrom.sortHand();
-		pTo.sortHand();
-		pFrom.dropPairs();
-		pTo.dropPairs();
-	}
 	public static void game(ArrayList<Player> players, int i) {
+		
 		int to = i % NUM_OF_PLAYERS;
 		int from = (i + 1) % NUM_OF_PLAYERS;
 		int toIndex = players.get(to).getIndex();
@@ -86,7 +36,7 @@ public class VariantOne extends OldMaid {
 				toIndex = fromIndex;
 				fromIndex = toIndex;
 			}
-			System.out.println("Player" + toIndex + " and Player" + fromIndex + " win");
+			System.out.println("[CONGRATULATIONS!]Player" + toIndex + " and Player" + fromIndex + " win");
 			NUM_OF_PLAYERS -= 2;
 			//seems that removal will call trim automatically, hence from -> to
 			players.remove(from);
@@ -94,20 +44,18 @@ public class VariantOne extends OldMaid {
 			players.trimToSize();
 		} else {
 			if(players.get(from).getHand().size() == 0) {
-				System.out.println("Player" + fromIndex + " wins");
+				System.out.println("[CONGRATULATIONS!]Player" + fromIndex + " wins");
 				NUM_OF_PLAYERS--;
 				players.remove(from);
 				players.trimToSize();
 			} else if(players.get(to).getHand().size() == 0) {
-				System.out.println("Player" + toIndex + " wins");
+				System.out.println("[CONGRATULATIONS!]Player" + toIndex + " wins");
 				NUM_OF_PLAYERS--;
 				players.remove(to);
 				players.trimToSize();
 			}
 		}
 	}
-	static int NUM_OF_PLAYERS = 4;
-
 	public static void main(String [] args) {
 
 		DeckOfCards deckOfCards = new DeckOfCards();
@@ -138,23 +86,11 @@ public class VariantOne extends OldMaid {
 		cardsOutput(players);
 
 		// Game start
-		// System.out.println("Game start");
+		System.out.println("VariantOne Game start");
 
 		int i = 0;
 		Boolean IS_OVER = false;
 
-		while(!IS_OVER) {
-
-			game(players, i);
-			if(NUM_OF_PLAYERS < 4) {
-				IS_OVER = true;
-			}
-			i++;
-		}
-		// System.out.println("Basic game over");
-		// System.out.println("Continue");
-
-		IS_OVER = false;
 		while(!IS_OVER) {
 
 			game(players, i);
@@ -163,7 +99,6 @@ public class VariantOne extends OldMaid {
 			}
 			i++;
 		}
-		// System.out.println("Bonus game over");
 		System.out.println("OldMaid VariantOne Game Over");
 
 	}
