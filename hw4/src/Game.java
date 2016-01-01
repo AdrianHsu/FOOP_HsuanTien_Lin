825ba232f3df4f6584b7627c01a3a417dc41d641
AdrianHsu <adrianhsu1995@gmail.com>
2016-01-02 00:28:25 +0800

    hw4 first commit

---
+++ hw4/src/Game.java
@@ -0,0 +1,8 @@
+public class Game {
+	public static void main(String [] args) {
+
+		POOCasino game = new POOCasino();
+		
+		System.out.println("Hello, hw4");
+	}
+}
---
+++ hw4/src/POOCasino.java
@@ -0,0 +1,8 @@
+import foop.Hand;
+import foop.Card;
+import foop.Player;
+
+public class POOCasino {
+
+
+}
